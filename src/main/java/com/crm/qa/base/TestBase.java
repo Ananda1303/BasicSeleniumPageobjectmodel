448f2680472c7a209b67b2a1be39bff9baef3b4a
Ananda1303 <151882106+Ananda1303@users.noreply.github.com>
2024-01-09 13:33:24 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/anand/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "/Users/anand/Downloads/chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
